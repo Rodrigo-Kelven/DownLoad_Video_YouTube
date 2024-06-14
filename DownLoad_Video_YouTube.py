890f9c077930bcf6928a063b22993e6b33a8f6e1
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2024-06-13 21:11:19 -0300

    Atualizar o DownLoad_Video_YouTube.py

--- DownLoad_Video_YouTube.py
+++ DownLoad_Video_YouTube.py
@@ -1,9 +1,11 @@
-import yt_dlp
+import yt_dlp 
 
+#faz um input da URL do vídeo que será baixado
 url = input("Enter the YouTube video URL: ")
 
 ydl_opts = {}
 
+#salva todos os vídeos baixados
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
 
