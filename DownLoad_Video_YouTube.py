145ac66541b87bcf46e32de750f57922ab75b4c4
Rodrigo_Kelven <92493747+Rodrigo-Kelven@users.noreply.github.com>
2024-06-19 00:50:09 -0300

    Atualizar o DownLoad_Video_YouTube.py

--- DownLoad_Video_YouTube.py
+++ DownLoad_Video_YouTube.py
@@ -1,11 +1,22 @@
-import yt_dlp 
+import os
+import yt_dlp
 
-#faz um input da URL do vídeo que será baixado
+# Faz um input da URL do vídeo que será baixado
 url = input("Enter the YouTube video URL: ")
 
-ydl_opts = {}
+# Nome da pasta onde as músicas serão salvas
+nome_pasta = "musicas"
 
-#salva todos os vídeos baixados
+# Criando a pasta se ela não existir
+if not os.path.exists(nome_pasta):
+    os.mkdir(nome_pasta)
+
+# Opções do yt-dlp para especificar o diretório de saída
+ydl_opts = {
+    'outtmpl': os.path.join(nome_pasta, '%(title)s.%(ext)s')  # Define o caminho de saída com o nome do arquivo
+}
+
+# Salva todos os vídeos baixados na pasta especificada
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url])
 
